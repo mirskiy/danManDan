c2efa6a30cd617ec426e0e3c73ad334a8099dfb2
Daniel Mirsky <mirskiy@gmail.com>
2015-09-30 09:41:03 -0400

    reformatted urls.py

--- dj/urls.py
+++ dj/urls.py
@@ -13,13 +13,16 @@ admin.autodiscover()
 # You can also change the ``home`` view to add your own functionality
 # to the project's homepage.
 
-urlpatterns = i18n_patterns("",
+urlpatterns = i18n_patterns(
+    "",
     # Change the admin prefix here to use an alternate URL for the
     # admin interface, which would be marginally more secure.
     ("^admin/", include(admin.site.urls)),
 )
 
-urlpatterns += patterns('',
+urlpatterns += patterns(
+    '',     # This is used to prepend view name or something, don't remove
+    # the first parameter is usually on the same line as patterns, but that causes too many warnings in pycharm
 
     # We don't want to presume how your homepage works, so here are a
     # few patterns you can use to set it up.
